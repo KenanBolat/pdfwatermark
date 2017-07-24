2d6f3bbbb2feda32d8e5a2036423ffd4bce334bd
KenanBolat <kenan23@gmail.com>
2017-07-24 13:50:18 +0300

    Update001

---
+++ main.py
@@ -0,0 +1,5 @@
+import os
+import watermark
+pdf_one = watermark.watermark("Fname")
+pdf_two = watermark.watermark("Fname2","A7")
+
--- watermark.py
+++ watermark.py
@@ -12,12 +12,31 @@ import datetime
 import time
 import reportlab
 
-Start = datetime.datetime.now()
-
-
-End = datetime.datetime.now()
 
 
 
+class watermark:
+    """ A watermark class customized with the reportlab module.
+     Attributes:
+        checkpage_type
+        watermark_it
+    """
+    _papertype = {"A4":[210,297], "A0": [841, 1189], "A1": [594, 841], "A2": [420, 594], "A3": [297, 420], "A5": [148,210]}
 
-print End-Start
+    def __init__(self, fname, pagetype="A4"):
+        self.fname = fname
+        self.pagetype = pagetype
+
+    def checkpage_type(self):
+        if self.pagetype != "A4":
+            raise RuntimeError('Page size must be A4 for this version of watermarking.')
+        else:
+            return True
+    def getPageExtend(self):
+        py
+
+    def watermark_it(self,word):
+        self.checkpage_type()
+        self.watermarktext = word
+        return True
+
