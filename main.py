33b8a761ce6fc57a2b4ec4be038a39f833b6f60d
KenanBolat <kenan23@gmail.com>
2017-07-24 15:40:32 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import watermark
-pdf_one = watermark.watermark("Fname")
-pdf_two = watermark.watermark("Fname2","A7")
+# pdf_one = watermark.watermark("/home/knn/Desktop/pdfwatermark/sample/trial.portre.0.1.pdf")
+pdf_two = watermark.watermark("/home/knn/Desktop/pdfwatermark/sample/trial.landscape.0.1.pdf","A4")
+
+pdf_two._test()
 
