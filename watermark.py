dcbbaa63840683b3f0bd6fe8affb9593552abb36
KenanBolat <kenan23@gmail.com>
2017-07-24 11:40:03 +0300

    Update001

---
+++ watermark.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Author      : Kenan Bolat
+Purpose     : Insert Watermark to a pdf document
+Date        : 170724
+Version     : 0.0.1
+License    : GNU GPLv3
+"""
+
+import os
+import datetime
+import time
+import reportlab
+
+Start = datetime.datetime.now()
+
+
+End = datetime.datetime.now()
+
+
+
+
+print End-Start
